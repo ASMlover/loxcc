62a5dd5f38c70a71789846846ded5729ec4163fb
ASMlover <asmlover@126.com>
2022-03-05 01:11:02 +0800

    :construction: chore(chunk): updated the definitions of chunk for bytecode of loxcc

--- bytecc_chunk.hh
+++ bytecc_chunk.hh
@@ -40,23 +40,23 @@ enum class Code : u8_t {
 #include "bytecc_codes.hh"
 };
 
-inline u8_t operator+(Code a, Code b) {
+inline u8_t operator+(Code a, Code b) noexcept {
   return Xt::as_type<u8_t>(a) + Xt::as_type<u8_t>(b);
 }
 
-inline u8_t operator-(Code a, Code b) {
+inline u8_t operator-(Code a, Code b) noexcept {
   return Xt::as_type<u8_t>(a) - Xt::as_type<u8_t>(b);
 }
 
-inline Code operator+(Code c, u8_t n) {
+inline Code operator+(Code c, u8_t n) noexcept {
   return Xt::as_type<Code>(Xt::as_type<u8_t>(c) + n);
 }
 
-inline Code operator-(Code c, u8_t n) {
+inline Code operator-(Code c, u8_t n) noexcept {
   return Xt::as_type<Code>(Xt::as_type<u8_t>(c) - n);
 }
 
-inline std::ostream& operator<<(std::ostream& out, Code c) {
+inline std::ostream& operator<<(std::ostream& out, Code c) noexcept {
   return out << Xt::as_type<int>(c);
 }
 
@@ -65,24 +65,24 @@ class Chunk final : private UnCopyable {
   std::vector<int> lines_;
   std::vector<Value> constants_;
 public:
-  template <typename T> inline void write(T code, int line) {
+  template <typename T> inline void write(T code, int lineno) noexcept {
     codes_.push_back(Xt::as_type<u8_t>(code));
-    lines_.push_back(line);
+    lines_.push_back(lineno);
   }
 
-  inline u8_t add_constant(const Value& value) {
+  inline u8_t add_constant(const Value& value) noexcept {
     constants_.push_back(value);
     return Xt::as_type<u8_t>(constants_.size() - 1);
   }
 
-  inline int codes_count(void) const { return Xt::as_type<int>(codes_.size()); }
-  inline const u8_t* codes(void) const { return codes_.data(); }
-  inline u8_t get_code(int i) const { return codes_[i]; }
-  inline void set_code(int i, u8_t c) { codes_[i] = c; }
-  inline int get_line(int i) const { return lines_[i]; }
-  inline int constants_count(void) const { return Xt::as_type<int>(constants_.size()); }
-  inline const Value* constants(void) const { return constants_.data(); }
-  inline const Value& get_constant(int i) const { return constants_[i]; }
+  inline int codes_count(void) const noexcept { return Xt::as_type<int>(codes_.size()); }
+  inline const u8_t* codes(void) const noexcept { return codes_.data(); }
+  inline u8_t get_code(int i) const noexcept { return codes_[i]; }
+  inline void set_code(int i, u8_t c) noexcept { codes_[i] = c; }
+  inline int get_line(int i) const noexcept { return lines_[i]; }
+  inline int constants_count(void) const noexcept { return Xt::as_type<int>(constants_.size()); }
+  inline const Value* constants(void) const noexcept { return constants_.data(); }
+  inline const Value& get_constant(int i) const noexcept { return constants_[i]; }
 
   inline void iter_constants(std::function<void (const Value&)>&& visitor) {
     for (auto& c : constants_)

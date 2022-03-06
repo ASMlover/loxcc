221e2fe944c2d2a857267789a4ecaaaf71588e2b
ASMlover <asmlover@126.com>
2022-03-07 01:49:39 +0800

    :construction: chore(chunk): updated the implementation of chunk

--- bytecc_chunk.cc
+++ bytecc_chunk.cc
@@ -29,6 +29,26 @@
 
 namespace loxcc::bytecc {
 
+inline int dis_compound(
+    Chunk* chunk, const char* prompt, int i, bool with_constant = false) noexcept {
+  auto c = chunk->get_code(i + 1);
+  std::fprintf(stdout, "%-16s %4d", prompt, c);
+  if (with_constant)
+    std::cout << " `" << chunk->get_constant(c) << "`";
+  std::cout << std::endl;
+
+  return i + 2;
+}
+
+inline int dis_simple(Chunk* chunk, const char* prompt, int i, int n = 0) noexcept {
+  std::cout << prompt;
+  if (n > 0)
+    std::cout << "_" << n;
+  std::cout << std::endl;
+
+  return i + 1;
+}
+
 void Chunk::dis(const str_t& name) {
   std::cout << "========= [" << name << "] =========" << std::endl;
   for (int i = 0; i < codes_count();)
@@ -36,29 +56,11 @@ void Chunk::dis(const str_t& name) {
 }
 
 int Chunk::dis_ins(int offset) {
-  auto const_ins = [](Chunk* c, const char* s, int i) -> int {
-    u8_t constant = c->get_code(i + 1);
-    fprintf(stdout, "%-16s %4d ", s, constant);
-    std::cout << "`" << c->get_constant(constant) << "`" << std::endl;
-    return i + 2;
-  };
   auto const_insN = [](Chunk* c, const str_t& s, int i, int n) -> int {
     u8_t constant = c->get_code(i + 1);
     fprintf(stdout, "%s_%-*d %4d ", s.c_str(), 15 - Xt::as_type<int>(s.size()), n, constant);
     std::cout << "`" << c->get_constant(constant) << "`" << std::endl;
     return i + 2;
-  };
-  auto simp_ins = [](const str_t& s, int i) -> int {
-    std::cout << s << std::endl;
-    return i + 1;
-  };
-  auto simp_insN = [](const str_t& s, int i, int n) -> int {
-    std::cout << s << n << std::endl;
-    return i + 1;
-  };
-  auto byte_ins = [](Chunk* c, const char* s, int i) -> int {
-    fprintf(stdout, "%-16s %4d\n", s, c->get_code(i + 1));
-    return i + 1;
   };
   auto jump_ins = [](Chunk* c, const char* s, int i, int sign) -> int {
     u16_t jump = Xt::as_type<u16_t>((c->get_code(i + 1) << 8) | c->get_code(i + 2));
@@ -71,35 +73,41 @@ int Chunk::dis_ins(int offset) {
     std::cout << "   | ";
   else
     fprintf(stdout, "%4d ", lines_[offset]);
+
+#define COMPOUND(x)     return dis_compound(this, #x, offset)
+#define COMPOUND2(x, b) return dis_compound(this, #x, offset, (b))
+#define SIMPLE(x)       return dis_simple(this, #x, offset)
+#define SIMPLE2(x, n)   return dis_simple(this, #x, offset, (n))
+
   switch (Code c = Xt::as_type<Code>(codes_[offset])) {
-  case Code::CONSTANT: return const_ins(this, "CONSTANT", offset);
-  case Code::NIL: return simp_ins("NIL", offset);
-  case Code::TRUE: return simp_ins("TRUE", offset);
-  case Code::FALSE: return simp_ins("FALSE", offset);
-  case Code::POP: return simp_ins("POP", offset);
-  case Code::DEF_GLOBAL: return const_ins(this, "DEF_GLOBAL", offset);
-  case Code::GET_GLOBAL: return const_ins(this, "GET_GLOBAL", offset);
-  case Code::SET_GLOBAL: return const_ins(this, "SET_GLOBAL", offset);
-  case Code::GET_LOCAL: return byte_ins(this, "GET_LOCAL", offset);
-  case Code::SET_LOCAL: return byte_ins(this, "SET_LOCAL", offset);
-  case Code::GET_UPVALUE: return byte_ins(this, "GET_UPVALUE", offset);
-  case Code::SET_UPVALUE: return byte_ins(this, "SET_UPVALUE", offset);
-  case Code::GET_ATTR: return const_ins(this, "GET_ATTR", offset);
-  case Code::SET_ATTR: return const_ins(this, "SET_ATTR", offset);
-  case Code::GET_SUPER: return const_ins(this, "GET_SUPER", offset);
-  case Code::EQ: return simp_ins("EQ", offset);
-  case Code::NE: return simp_ins("NE", offset);
-  case Code::GT: return simp_ins("GT", offset);
-  case Code::GE: return simp_ins("GE", offset);
-  case Code::LT: return simp_ins("LT", offset);
-  case Code::LE: return simp_ins("LE", offset);
-  case Code::ADD: return simp_ins("ADD", offset);
-  case Code::SUB: return simp_ins("SUB", offset);
-  case Code::MUL: return simp_ins("MUL", offset);
-  case Code::DIV: return simp_ins("DIV", offset);
-  case Code::NOT: return simp_ins("NOT", offset);
-  case Code::NEG: return simp_ins("NEG", offset);
-  case Code::PRINT: return simp_ins("PRINT", offset);
+  case Code::CONSTANT: COMPOUND2(CONSTANT, true);
+  case Code::NIL: SIMPLE(NIL);
+  case Code::TRUE: SIMPLE(TRUE);
+  case Code::FALSE: SIMPLE(FALSE);
+  case Code::POP: SIMPLE(POP);
+  case Code::DEF_GLOBAL: COMPOUND2(DEF_GLOBAL, true);
+  case Code::GET_GLOBAL: COMPOUND2(GET_GLOBAL, true);
+  case Code::SET_GLOBAL: COMPOUND2(SET_GLOBAL, true);
+  case Code::GET_LOCAL: COMPOUND(GET_LOCAL);
+  case Code::SET_LOCAL: COMPOUND(SET_LOCAL);
+  case Code::GET_UPVALUE: COMPOUND(GET_UPVALUE);
+  case Code::SET_UPVALUE: COMPOUND(SET_UPVALUE);
+  case Code::GET_ATTR: COMPOUND2(GET_ATTR, true);
+  case Code::SET_ATTR: COMPOUND2(SET_ATTR, true);
+  case Code::GET_SUPER: COMPOUND2(GET_SUPER, true);
+  case Code::EQ: SIMPLE(EQ);
+  case Code::NE: SIMPLE(NE);
+  case Code::GT: SIMPLE(GT);
+  case Code::GE: SIMPLE(GE);
+  case Code::LT: SIMPLE(LT);
+  case Code::LE: SIMPLE(LE);
+  case Code::ADD: SIMPLE(ADD);
+  case Code::SUB: SIMPLE(SUB);
+  case Code::MUL: SIMPLE(MUL);
+  case Code::DIV: SIMPLE(DIV);
+  case Code::NOT: SIMPLE(NOT);
+  case Code::NEG: SIMPLE(NEG);
+  case Code::PRINT: SIMPLE(PRINT);
   case Code::JUMP: return jump_ins(this, "JUMP", offset, 1);
   case Code::JUMP_IF_FALSE: return jump_ins(this, "JUMP_IF_FALSE", offset, 1);
   case Code::LOOP: return jump_ins(this, "LOOP", offset, -1);
@@ -111,7 +119,7 @@ int Chunk::dis_ins(int offset) {
   case Code::CALL_5:
   case Code::CALL_6:
   case Code::CALL_7:
-  case Code::CALL_8: return simp_insN("CALL_", offset, c - Code::CALL_0);
+  case Code::CALL_8: SIMPLE2(CALL, c - Code::CALL_0);
   case Code::INVOKE_0:
   case Code::INVOKE_1:
   case Code::INVOKE_2:
@@ -130,12 +138,12 @@ int Chunk::dis_ins(int offset) {
   case Code::SUPER_6:
   case Code::SUPER_7:
   case Code::SUPER_8: return const_insN(this, "SUPER_", offset, c - Code::SUPER_0);
-  case Code::CLOSURE: return const_ins(this, "CLOSURE", offset);
-  case Code::CLOSE_UPVALUE: return simp_ins("CLOSE_UPVALUE", offset);
-  case Code::RETURN: return simp_ins("RETURN", offset);
-  case Code::CLASS: return const_ins(this, "CLASS", offset);
-  case Code::SUBCLASS: return simp_ins("SUBCLASS", offset);
-  case Code::METHOD: return const_ins(this, "METHOD", offset);
+  case Code::CLOSURE: COMPOUND2(CLOSURE, true);
+  case Code::CLOSE_UPVALUE: SIMPLE(CLOSE_UPVALUE);
+  case Code::RETURN: SIMPLE(RETURN);
+  case Code::CLASS: COMPOUND2(CLASS, true);
+  case Code::SUBCLASS: SIMPLE(SUBCLASS);
+  case Code::METHOD: COMPOUND2(METHOD, true);
   default: std::cerr << "UNKNOWN CODE: " << c << std::endl; break;
   }
   return offset + 1;
